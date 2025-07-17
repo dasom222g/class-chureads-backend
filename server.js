854c5052d334e879a0e14f3113d9217b1a78a5ef
dasom222g <dasom228@gmail.com>
2025-07-17 12:21:08 +0900

    Feat: Add tagService with openai

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { testTagGenerate } from "./services/tagService.js";
 
 // 환경변수 로드
 // 전역으로 로드해서 모든 node.js모듈 내에서 접근가능
@@ -10,4 +11,5 @@ const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log("Server at..", PORT);
+  testTagGenerate();
 });

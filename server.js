79cf0d6fcee047f5a3cc05a994ee17378f6f6f8c
dasom222g <dasom228@gmail.com>
2025-07-17 13:54:31 +0900

    feat: Add router

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { testTagGenerate } from "./services/tagService.js";
+import postRouter from "./routes/posts.js";
 
 // 환경변수 로드
 // 전역으로 로드해서 모든 node.js모듈 내에서 접근가능
@@ -9,7 +10,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+// Json 파싱 설정
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 라우터 미들웨어 등록
+app.use("/posts", postRouter);
+
 app.listen(PORT, () => {
   console.log("Server at..", PORT);
-  testTagGenerate();
+  // testTagGenerate();
 });
